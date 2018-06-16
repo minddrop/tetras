e63f982e386bf8060335a40bb38b0e949decd27d
ryu <ryooooooo1@gmail.com>
2018-06-16 21:07:54 +0900

    first commit

---
+++ Assets/ColorController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorController : MonoBehaviour
+{
+
+
+    public Color red = Color.red;
+    Renderer rend;
+    Vector3 start;
+    public GameObject gameObject;
+    private Vector3 distance;
+
+
+    void Start ()
+	{
+	    int a = 3;
+        rend = GetComponent<Renderer>();
+        Debug.Log(rend.material.color);
+        start = this.transform.position;
+
+	}
+
+    void Update()
+    {
+        distance = this.transform.position - gameObject.transform.position;
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rend.material.color = Color.red;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rend.material.color = new Vector4(1, 1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.B))
+        {
+            this.transform.position = start;
+        }
+        rend.material.color = new Vector4(distance.x/10,distance.y/10,distance.z/10,1);
+    }
+	
+
+}
